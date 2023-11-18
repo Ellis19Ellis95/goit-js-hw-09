bfc859d5cbc327e2af8c4665439dd6d314d657a8
Ellis19Ellis95 <101639151+Ellis19Ellis95@users.noreply.github.com>
2023-11-18 22:32:58 +0200

    7

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,36 +1,29 @@
-// Отримання посилань на кнопки та body
 const startButton = document.querySelector('[data-start]');
 const stopButton = document.querySelector('[data-stop]');
 const body = document.body;
 
-// Функція для генерації випадкового кольору
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
 }
 
 let intervalId = null;
+let lastColor = null;
 
-// Функція для зміни кольору фону
 function changeColor() {
-  body.style.backgroundColor = getRandomHexColor();
+  const newColor = getRandomHexColor();
+  body.style.backgroundColor = newColor;
+  lastColor = newColor;
 }
 
-// Обробник подій для кнопки "Start"
 startButton.addEventListener('click', () => {
-  // Вимкнення кнопки "Start"
   startButton.disabled = true;
-  
-  // Запуск інтервалу зі зміною кольору кожну секунду
   intervalId = setInterval(changeColor, 1000);
 });
 
-// Обробник подій для кнопки "Stop"
 stopButton.addEventListener('click', () => {
-  // Включення кнопки "Start"
   startButton.disabled = false;
-  
-  // Зупинка інтервалу зі зміною кольору
   clearInterval(intervalId);
-  // Скидання кольору на початковий
-  body.style.backgroundColor = '';
+  if (lastColor) {
+    body.style.backgroundColor = lastColor;
+  }
 });
