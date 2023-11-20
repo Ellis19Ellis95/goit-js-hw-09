9c99236307ee865fad73e0c062eb865b63634cbe
Ellis19Ellis95 <101639151+Ellis19Ellis95@users.noreply.github.com>
2023-11-20 16:41:00 +0200

    35

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,33 @@
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        // Fulfill
+        resolve({ position, delay });
+      } else {
+        // Reject
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
+
+document.querySelector('.form').addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const formData = new FormData(event.target);
+  const firstDelay = parseInt(formData.get('delay'), 10);
+  const step = parseInt(formData.get('step'), 10);
+  const amount = parseInt(formData.get('amount'), 10);
+
+  for (let i = 0; i < amount; i++) {
+    createPromise(i + 1, firstDelay + step * i)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+  }
+});
